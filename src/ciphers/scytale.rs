3a644578c9d5dcffd61c3b99cf026646c7e86236
mowzhja <mowzhja@protonmail.ch>
2021-04-10 17:21:19 +0200

    added padding function to utils and cleaned up scytale (a test still doesn't pass!)

--- src/ciphers/scytale.rs
+++ src/ciphers/scytale.rs
@@ -1,63 +1,39 @@
-use super::utils::clean_input;
+use super::utils::{clean_input, pad};
 
-// TODO make the scytale into a struct to keep data about it
-// TODO (like the number of rows and columns)
-fn build_scytale(pt: &str, diameter: u8) -> Vec<Vec<char>> {
-    // attack at dawn
-    // diameter 3
-    // => a t t
-    //    a c k
-    //    a t d
-    //    a w n
-    // lucky for me, Rust is awesome and it has chunks()!
-    let v_chars = pt.chars().collect::<Vec<char>>();
-    let chunks = v_chars.chunks(diameter as usize).collect::<Vec<_>>();
+fn get_diameter(s: &str, length: usize) -> usize {
+    let padded_s = pad(s, length as u32);
 
-    chunks
-        .iter()
-        .map(|v| v.to_vec())
-        .collect::<Vec<Vec<char>>>()
+    padded_s.len() / length
+
 }
 
-pub fn encrypt(plaintext: &str, key: u8) -> String {
+fn wrap_around_scytale(text: &str, n: usize) -> String {
+    let mut unwinded = String::new();
+    let l = text.len();
+
+    for i in 0..=l {
+        for j in (i..l).step_by(n) {
+            unwinded.push(text.chars().nth(j).unwrap());
+        }
+    }
+    // TODO can i get the proper length in the loops above
+    // without having to truncate?
+    unwinded.truncate(l);
+
+    unwinded
+}
+
+pub fn encrypt(plaintext: &str, length: usize) -> String {
     let clean_plaintext = clean_input(plaintext);
-    let scytale = build_scytale(&clean_plaintext, key);
-    let nrows = scytale.len();
-    println!("{:?}", scytale);
 
-    let mut ciphertext = String::new();
-
-    // FIXME this fails for a very obvious reason (look at the output of println)
-    for i in 0..key as usize {
-        for j in 0..nrows {
-            ciphertext.push(scytale[j][i]);
-        }
-    }
-
-    ciphertext
+    wrap_around_scytale(&clean_plaintext, length)
 }
 
-// => the key provided doesn't work for decryption!! => the scytale matrix is not
-// the same => it's transposed of the one used for encryption 
-// => num of rows = num of columns!
-pub fn decrypt(ciphertext: &str, key: u8) -> String {
+pub fn decrypt(ciphertext: &str, length: usize) -> String {
     let clean_ciphertext = clean_input(ciphertext);
-    // this line should be modified
-    // the lines below are a pretty ugly hack 
-    // TODO fix these (best way is probably making a struct as above)
-    let scytale = build_scytale(&clean_ciphertext, key);
-    let nrows = scytale.len();
-    let scytale = build_scytale(&clean_ciphertext, nrows as u8);
+    let diameter = get_diameter(&clean_ciphertext, length);
 
-    let mut plaintext = String::new();
-
-    for i in 0..nrows {
-        for j in 0..key as usize {
-            plaintext.push(scytale[j][i]);
-        }
-    }
-
-    plaintext
+    wrap_around_scytale(&clean_ciphertext, diameter)
 }
 
 #[cfg(test)]
@@ -66,76 +42,42 @@ mod tests {
 
     #[test]
     #[should_panic]
-    fn test_zero_diameter() {
-        build_scytale("this should not work", 0);
-    }
-
-    #[test]
-    fn test_build_scytale() {
-        // from https://en.wikipedia.org/wiki/Scytale
-        let clean_plaintext = clean_input("I am hurt very badly help");
-        let key = 5;
-        let expected_scytale: Vec<Vec<char>> = vec![
-            ['I', 'A', 'M', 'H', 'U'].to_vec(),
-            ['R', 'T', 'V', 'E', 'R'].to_vec(),
-            ['Y', 'B', 'A', 'D', 'L'].to_vec(),
-            ['Y', 'H', 'E', 'L', 'P'].to_vec(),
-        ];
-        let scytale = build_scytale(&clean_plaintext, key);
-
-        assert_eq!(scytale, expected_scytale);
-
-        // edge cases
-        let clean_plaintext = clean_input("testing edge");
-        let key = 1;
-        let expected_scytale: Vec<Vec<char>> = vec![
-            ['T'].to_vec(),
-            ['E'].to_vec(),
-            ['S'].to_vec(),
-            ['T'].to_vec(),
-            ['I'].to_vec(),
-            ['N'].to_vec(),
-            ['G'].to_vec(),
-            ['E'].to_vec(),
-            ['D'].to_vec(),
-            ['G'].to_vec(),
-            ['E'].to_vec(),
-        ];
-        let scytale = build_scytale(&clean_plaintext, key);
-
-        assert_eq!(scytale, expected_scytale);
-
-        let clean_plaintext = clean_input("short pt");
-        let key = 100;
-        let expected_scytale: Vec<Vec<char>> = vec![
-            ['S', 'H', 'O', 'R', 'T', 'P', 'T'].to_vec(),
-        ];
-        let scytale = build_scytale(&clean_plaintext, key);
-
-        assert_eq!(scytale, expected_scytale);
+    fn test_zero_length() {
+        encrypt("this should fail", 0);
     }
 
     #[test]
     fn test_known_pairs() {
         // from https://en.wikipedia.org/wiki/Scytale
         let plaintext = "I am hurt very badly help";
-        let key = 5;
+        let length = 5;
         let ciphertext = String::from("Iryyatbhmvaehedlurlp");
 
-        assert_eq!(ciphertext.to_uppercase(), encrypt(plaintext, key));
-        assert_eq!(clean_input(plaintext), decrypt(&ciphertext, key));
-        
+        assert_eq!(ciphertext.to_uppercase(), encrypt(plaintext, length));
+
+        assert_eq!(clean_input(plaintext), decrypt(&ciphertext, length));
+
+        // from 'Programming in Cryptol', page 30
+        let plaintext = "attackatdawn";
+        let length = 4; // in the book the diameter is 3
+        let ciphertext = String::from("ACDTKATAWATN");
+
+        assert_eq!(ciphertext, encrypt(plaintext, length));
+
+        assert_eq!(clean_input(plaintext), decrypt(&ciphertext, length));
     }
 
     #[test]
     fn test_correct() {
-        let plaintext = "for sparta, dude!";
+        let plaintext = String::from("Iamhurtverybadly");
 
-        for diameter in 1..=plaintext.len() {
-            assert_eq!(
-                clean_input(plaintext),
-                decrypt(&encrypt(plaintext, diameter as u8), diameter as u8)
-            );
+        // FIXME something very subtle is going on here...
+        for length in 1..=plaintext.len() {
+                println!("{}", length);
+                assert_eq!(
+                    plaintext.to_uppercase(),
+                    decrypt(&encrypt(&plaintext, length), length)
+                );
         }
     }
 }
--- src/ciphers/utils.rs
+++ src/ciphers/utils.rs
@@ -12,6 +12,20 @@ pub fn clean_input(input: &str) -> String {
         .to_uppercase()
 }
 
+// XXX this is actually pretty useless, may have to delete it
+/// Pad a string until it's length is a multiple of n (the closest multiple
+/// of n with respect to the string's initial length)
+pub fn pad(data: &str, n: u32) -> String {
+    let mut ret = String::from(data);
+
+    // usually n < data.len() so this while will be pretty fast
+    while ret.len() % (n as usize) != 0 {
+        ret.push('.');
+    }
+
+    ret
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -27,5 +41,22 @@ mod tests {
         assert_eq!(expected, clean_input(&input));
 
         assert_eq!("".to_string(), clean_input(""));
+    }
+
+    #[test]
+    fn test_pad() {
+        let input = String::from("testing");
+        let n = 4;
+        let expected = String::from("testing.");
+        assert_eq!(expected, pad(&input, n));
+
+        let input = String::from("lololo");
+        let n = 3;
+        assert_eq!(input, pad(&input, n));
+
+        let input = String::from("te");
+        let n = 12;
+        let expected = String::from("te..........");
+        assert_eq!(expected, pad(&input, n));
     }
 }
